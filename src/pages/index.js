5cfb67ea7e8f0d3662e59c8989417d76a5b04d1f
Anvar <rfadxb@gmail.com>
2018-07-10 17:24:46 +0400

    initialized 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react";
 export default () =>
  <div style={{ color: `tomato` }}>
    <h1>Hello Gatsby!</h1>
+   <h2>Anvar First Project</h2>
    <p>What a world.</p>
    <img src="https://source.unsplash.com/random/400x200" alt="" />
  </div>
